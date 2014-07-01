0c6836a7951266e06f32ff3c59478701e2e98c97
Derek Schaefer <derek.schaefer@gmail.com>
2014-06-30 19:31:55 -0500

    Right vs Left

--- lib/categoric/either.rb
+++ lib/categoric/either.rb
@@ -7,16 +7,32 @@ module Categoric
       if side then Right.join value
       else Left.join value end
     end
+
+    def right?
+      false
+    end
+
+    def left?
+      false
+    end
   end
 
   class Right < Either
     def any?
       true
     end
+
+    def right?
+      true
+    end
   end
 
   class Left < Either
     def any?
+      true
+    end
+
+    def left?
       true
     end
   end
--- spec/lib/categoric/either_spec.rb
+++ spec/lib/categoric/either_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 describe Categoric::Either do
+  def match_class(e)
+    case e
+    when Right then :right
+    when Left then :left
+    end
+  end
+
   describe 'Either' do
     it { expect(Either(->(n) { n > 0 }, 2)).to eq Right(2) }
     it { expect(Either(->(n) { n > 0 }, 0)).to eq Left(0) }
     it { expect(Either(->(n) { n > 0 }, -2)).to eq Left(-2) }
     it { expect(Either(->(n) { n > 0 }) { 2 + 2 }).to eq Right(4) }
     it { expect(Either(->(n) { n > 0 }) { -2 * 2}).to eq Left(-4) }
+    it { expect(match_class Either(->(n) { n > 0 }, 2)).to eq :right }
+    it { expect(match_class Either(->(n) { n > 0 }, -2)).to eq :left }
   end
 
   describe 'Right' do
@@ -17,5 +26,15 @@ describe Categoric::Either do
   describe 'Left' do
     it { expect(Left 42).to eq Left(42) }
     it { expect(Left Left(42)).to eq Left(42) }
+  end
+
+  describe '#right?' do
+    it { expect(Either(->(n) { n > 0 }, 2).right?).to be true }
+    it { expect(Either(->(n) { n > 0 }, -2).right?).to be false }
+  end
+
+  describe '#left?' do
+    it { expect(Either(->(n) { n > 0 }, 2).left?).to be false }
+    it { expect(Either(->(n) { n > 0 }, -2).left?).to be true }
   end
 end
